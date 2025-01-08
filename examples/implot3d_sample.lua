59cac2e3ca3201d0436c780a8c6648f1cc4f2fd4
sonoro1234 <sonoroster@gmail.com>
2025-01-08 16:11:42 +0100

    implot3d_sample: now moves with time

--- examples/implot3d_sample.lua
+++ examples/implot3d_sample.lua
@@ -7,14 +7,15 @@ win.ig.ImPlot3D_CreateContext()
 local ffi = require"ffi"
 
 local xs1, ys1, zs1 = ffi.new("float[?]",1001),ffi.new("float[?]",1001),ffi.new("float[?]",1001)
+
+
+
+function win:draw(ig)
 for  i = 0,1001-1 do
     xs1[i] = i * 0.001;
     ys1[i] = 0.5 + 0.5 * math.cos(50 * (xs1[i] + win.ig.GetTime() / 10));
     zs1[i] = 0.5 + 0.5 * math.sin(50 * (xs1[i] + win.ig.GetTime() / 10));
 end
-
-
-function win:draw(ig)
     ig.ImPlot3D_ShowDemoWindow()
     ig.Begin("Ploters")
     if (ig.ImPlot3D_BeginPlot("Line Plot", ig.ImVec2(0,0))) then
